07cd078f27ea492d67899ad0a6411b7ca3fbbac9
chamidilk <chamidilk@gmail.com>
2019-11-30 08:07:09 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openPdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openPdfToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1037, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openPdfToolStripMenuItem
+            // 
+            this.openPdfToolStripMenuItem.Checked = true;
+            this.openPdfToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.openPdfToolStripMenuItem.Name = "openPdfToolStripMenuItem";
+            this.openPdfToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.openPdfToolStripMenuItem.Text = "open pdf";
+            this.openPdfToolStripMenuItem.Click += new System.EventHandler(this.openPdfToolStripMenuItem_Click);
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer1.Location = new System.Drawing.Point(0, 24);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.ShowToolbar = false;
+            this.pdfViewer1.Size = new System.Drawing.Size(1037, 504);
+            this.pdfViewer1.TabIndex = 1;
+            this.pdfViewer1.Load += new System.EventHandler(this.pdfViewer1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 528);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openPdfToolStripMenuItem;
+        private PdfiumViewer.PdfViewer pdfViewer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfiumViewer;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openPdfToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                openFile(dialog.FileName);
+            }
+        }
+
+        private void openFile(string filepath)
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(filepath);
+            var stream = new MemoryStream(bytes);
+            PdfDocument pdfDocument = PdfDocument.Load(stream);
+            pdfViewer1.Document = pdfDocument;
+        }
+
+        private void pdfViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
